744e708455d8605f4dcd70526eb37bc1987ac949
Jing Li <jing.li08@sap.com>
2019-06-14 09:40:18 +0800

    shouhuan 4

--- main.py
+++ main.py
@@ -29,11 +29,14 @@ def refresh_item_page(item_url, window_num):
     browser.switch_to.window(browser.window_handles[window_num])
     snap()
 
-
     # refresh until it could buy
     while 1:
         try:
-            browser.find_element_by_xpath('//*[@id="J_headNav"]/div/div/div/a[5]').click()
+            # red airdots
+            # browser.find_element_by_xpath('//*[@id="J_headNav"]/div/div/div/a[5]').click()
+            # shouhuan4
+            # browser.find_element_by_xpath('//*[@id="J_headNav"]/div/div/div[2]/a[6]').click()
+            browser.find_element_by_css_selector("#J_headNav > div > div > div.right > a.btn.btn-small.btn-primary")
         except:
             snap()
             continue
@@ -88,22 +91,43 @@ def check_out_bill(window_num, item_num):
         j += 1
 
     snap()
-    go_checkout_btn = browser.find_element_by_id('J_goCheckout')
-    browser.execute_script("arguments[0].click();", go_checkout_btn)
-    snap()
-    address_item = browser.find_element_by_xpath('//*[@id="J_addressList"]/div[1]')
-    browser.execute_script("arguments[0].click();", address_item)
 
-    browser.find_element_by_id('J_checkoutToPay').click()
+    while 1:
+        try:
+            go_checkout_btn = browser.find_element_by_id('J_goCheckout')
+            browser.execute_script("arguments[0].click();", go_checkout_btn)
+            break
+        except:
+            snap()
+            continue
+    snap()
+
+    while 1:
+        try:
+            address_item = browser.find_element_by_xpath('//*[@id="J_addressList"]/div[1]')
+            browser.execute_script("arguments[0].click();", address_item)
+            browser.find_element_by_id('J_checkoutToPay').click()
+            break
+        except:
+            snap()
+            continue
+    snap()
 
 
 if __name__ == '__main__':
 
-    item_url = "https://www.mi.com/redmiairdots/?cfrom=search"
+    # item_url = "https://www.mi.com/redmiairdots/?cfrom=search"
+    item_url = "https://www.mi.com/shouhuan4/?cfrom=search"
     # item_url = "https://www.mi.com/camera-360/?cfrom=search"
 
     browser = webdriver.Chrome()
     login()
     refresh_item_page(item_url, 1)
-    check_out_bill(2, 3)
-    browser.find_element_by_xpath('//*[@id="J_weixin"]/img').click()
+    check_out_bill(2, 1)
+    while 1:
+        try:
+            browser.find_element_by_xpath('//*[@id="J_weixin"]/img').click()
+            break
+        except:
+            snap()
+            continue
